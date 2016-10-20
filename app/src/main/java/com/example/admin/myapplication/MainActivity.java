63a459148d7ddea968ccbc3ef797ff5973b4466c
dvAf34 <elisabeth_vas@hotmail.com>
2016-10-20 19:40:36 +0200

    j'ai changé un truc dans le main

---
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.admin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.text.TextWatcher;
+import android.view.animation.TranslateAnimation;
+import android.view.Menu;
+
+
+public class MainActivity extends AppCompatActivity {
+    Menu menu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
